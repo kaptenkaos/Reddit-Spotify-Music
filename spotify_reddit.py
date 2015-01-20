09a08f6131e94d907bb93fb79100fb60563498d3
kaptenkaos <kaptenkaos@gmail.com>
2015-01-20 10:02:48 +0100

    Update spotify_reddit.py
    Made it class-based.

--- spotify_reddit.py
+++ spotify_reddit.py
@@ -5,87 +5,113 @@ import spotipy
 import praw
 import HTMLParser
 import re
+import sys
 import spotipy.util as util
 from urlparse import urlparse
-'''
-Spotify Client id, get it when you create a app at spotify
-'''
-client = 'Client-ID'
-'''
-Spotify Client secret, get it when you create a app at spotify
-'''
-clientsecret = 'Client-Secret'
-'''
-Url to direct the first valid request to spotify, does not need to be a address that works.
-Intended to be the webpage of your application
-'''
-redirecturi='redirect-URL'
-'''
-Spotify username
-'''
-username = 'Spotify Username'
-'''
-Spotify playlist to add tracks to
-'''
-playlist_id = 'Playlist ID'
 
-def reddit_search():
-	r = praw.Reddit(user_agent='Reddit-Spoitfy-Music')
-	m_com = praw.helpers.comment_stream(r, 'all', limit=150)
-	while True:
-		try:
-			for comments in m_com:
-				h = HTMLParser.HTMLParser()
-				comment_body = h.unescape(comments.body)
-				search(comment_body)
-				
-				
-		except KeyboardInterrupt:
-			print('Keyboard Exit')
-			pass
-		except:
-			continue
-
-def search(body_comment):
-	urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', body_comment)
-	for i in urls:
-		search_spotify(i)
 		
-def search_spotify(i):
-	url_parse = urlparse(i)
-	if url_parse.netloc == 'open.spotify.com':
-		if '/track/' in url_parse.path:
-			ex_track = url_parse.path.rsplit('/',1)
-			clean_track = ex_track[1].replace(')', '')
-			print(clean_track)
-			spotify_track(clean_track)
-		else:
+class Reddit(): # Class for Reddit
+
+	def Get_comments(self, subreddit, c_limit):
+		'''
+		Stream comments from Reddit.
+		'''
+		Bot_name = ""
+		r = praw.Reddit(user_agent=Bot_name)
+		m_com = praw.helpers.comment_stream(r, subreddit, limit=c_limit)
+		while True:
+			try:
+				for comments in m_com:
+					h = HTMLParser.HTMLParser()
+					Comment_body = h.unescape(comments.body)
+					Reddit().Search_links(Comment_body)
+			except KeyboardInterrupt:
+				print('Keyboard Exit')
+				sys.exit()
+			except:
+				continue
+
+	def Search_links(self, Comment):
+		'''
+		Filter out all links from reddit_search and filter out the links you are after
+		'''
+		urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', Comment)
+		for i in urls:
+			url_parse = urlparse(i)
+			if "spotify.com" in url_parse.netloc:
+				print(urls)
+				if '/track/' in url_parse.path:
+					ex_track = url_parse.path.rsplit('/',1)
+					Clean_track = ex_track[1].replace(')', '')
+					Spotify().Spotify_Track(Clean_track)
+					
+				elif '/album/' in url_parse.path:
+					Ex_album = url_parse.path.rsplit('/',1)
+					Clean_album = Ex_album[1].replace(')', '')
+					Spotify().Spotify_Album(Clean_album)
+					
+				elif '/playlist/' in url_parse.path:
+					Ex_playlist = url_parse.path.rsplit('/',1)
+					Clean_playlist = Ex_playlist[1].replace(')', '')
+					Spotify().Spotify_Playlist(Clean_playlist)
+				else:
+					pass
+			else:
+				pass
+
+
+class Spotify(): # Spotify Class
+		def __init__(self):
+		
+			'''
+			Spotify Client id, get it when you create a app at spotify
+			'''
+			self.client = ''
+			'''
+			Spotify Client secret, get it when you create a app at spotify
+			'''
+			self.clientsecret = ''
+			'''
+			Url to direct the first valid request to spotify, does not need to be a address that works.
+			Intended to be the webpage of your application
+			'''
+			self.redirecturi = ''
+			'''
+			Spotify username
+			'''
+			self.username = ''
+			'''
+			Spotify playlist to add tracks to
+			'''
+			self.playlist_id = ''
+			
+		def Spotify_Track(self, track_id):
+			try:
+				track_id = [track_id]
+				print track_id
+				scope = 'playlist-modify-public'
+				token = util.prompt_for_user_token(self.username, scope, client_id=self.client, client_secret=self.clientsecret, redirect_uri=self.redirecturi)
+				if token:
+					sp = spotipy.Spotify(auth=token)
+					sp.trace = False
+					results = sp.user_playlist_add_tracks(self.username, self.playlist_id, track_id)
+					print results
+				else:
+					print "Can't get token for", username
+			except:
+				print('Something went wrong... :(')
 			pass
-	else:
-		pass
-
-def spotify_artist(artist):
-	sp = spotipy.Spotify()
-	track = sp.artist(artist)
-	print(track.get('name'))
-
-def spotify_track(track_id):
-	try:
-		track_id = [track_id]
-		print track_id
-		scope = 'playlist-modify-public'
-		token = util.prompt_for_user_token(username, scope, client_id=client, client_secret=clientsecret, redirect_uri=redirecturi)
-		if token:
-			sp = spotipy.Spotify(auth=token)
-			sp.trace = False
-			results = sp.user_playlist_add_tracks(username, playlist_id, track_id)
-			print results
-		else:
-			print "Can't get token for", username
-	except:
-		print('Something went wrong... :(')
-		pass
-
+			
+		def Spotify_Playlist(self, playlist_id):
+			# Will be coming.
+			print(playlist_id)
+			print('Function is not yet ready')
+		
+		def Spotify_Album(self, album_id):
+			# Will be coming
+			print(album_id)
+			print('Function is not yet ready')
 
 if __name__ == '__main__':
-	reddit_search()
+	# Choose Subreddit and numbers of post
+	Reddit().Get_comments('all', 150)
